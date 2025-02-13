d6a780b66c6a50db2e66b4993fb4b686952cfd6e
VishalYerne <vishaldyerne1985@gmail.com>
2025-02-13 18:05:35 +0530

    Abbb

--- main.tf
+++ main.tf
@@ -2,14 +2,13 @@ terraform {
   required_version = ">= 1.3.0"
 
   backend "s3" {
-    bucket         = "terraform-backend-bucket123"
-    key            = "terraform/state"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-table"
+    bucket = "terraform-backend-bucket123"
+    key    = "terraform/state"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
   region  = "us-east-1"
-  version = ">= 5.0" # Ensuring stability
+  version = ">= 5.0"
 }
