8dd806467dade0b4dfc9cbc0af8a517d3180dd43
VishalYerne <vishaldyerne1985@gmail.com>
2025-02-13 16:14:27 +0530

    I

--- main.tf
+++ main.tf
@@ -2,11 +2,9 @@ terraform {
   required_version = ">= 1.3.0"
 
   backend "s3" {
-    bucket = "terraform-backend-bucket"
+    bucket = "terraform-backend-bucket5"
     key    = "terraform/state"
     region = "us-east-1"
-    AWS_ACCESS_KEY_ID = "AKIATXN67EGQFLEWAOPB"
-    AWS_SECRET_ACCESS_KEY = "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"
   }
 }
 
