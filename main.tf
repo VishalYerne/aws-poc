fbbb37331c9ba57154f5394ca348e4fee7a5d9ca
VishalYerne <vishaldyerne1985@gmail.com>
2025-02-13 16:03:49 +0530

    G

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.3.0"
 
   backend "s3" {
-    bucket = "terraform-backend-bucket5"
+    bucket = "terraform-backend-bucket"
     key    = "terraform/state"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
 
