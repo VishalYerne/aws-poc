6149cbb55179f78a4d5d4963fe979a7443614d3b
VishalYerne <vishaldyerne1985@gmail.com>
2025-02-13 21:19:02 +0530

    Abcdef

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.3.0"
 
   backend "s3" {
-    bucket  = "my-terraform-test-bucket-1"
+    bucket  = "my-terraform-test-bucket-2"
     key     = "terraform.tfstate"  # Defines where the state file will be stored
     region  = "us-east-1"
   
