c46630e3ca760c5d4d6756431530698b1caf4c12
VishalYerne <vishaldyerne1985@gmail.com>
2025-02-14 15:17:16 +0530

    modifying s3_source

--- S3_source.tf
+++ S3_source.tf
@@ -3,11 +3,11 @@ resource "aws_s3_bucket" "source_bucket" {
   bucket = var.source_bucket_name
 }
 
-# Upload initial.csv to Source S3 Bucket
-resource "aws_s3_object" "initial_data" {
-  bucket       = aws_s3_bucket.source_bucket.bucket
-  key          = "initial_data.csv"
-  source       = "${path.module}/data/initial_data.csv"
-  content_type = "text/csv"
-  etag         = filemd5("${path.module}/data/initial_data.csv")  # Optional: Ensures updates trigger re-upload
-}
+
+# # Upload initial.csv to Source S3 Bucket
+# resource "aws_s3_object" "initial_data" {
+#   bucket       = aws_s3_bucket.source_bucket.bucket
+#   key          = "initial_data.csv"
+#   source       = "${path.module}/data/initial_data.csv"
+#   content_type = "text/csv"
+# }
