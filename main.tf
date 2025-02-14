57f66fdd6d1a5ef55d0cfdf4cb3bfc92ce216746
VishalYerne <vishaldyerne1985@gmail.com>
2025-02-14 12:15:24 +0530

    modifying main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.3.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.31.0"  # ✅ Correct way to specify version
+      version = ">= 5.31.0"
     }
   }
 }
