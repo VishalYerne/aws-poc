02fafb1c1984c03ae1ff95ae0becafdbb6e7e72a
VishalYerne <vishaldyerne1985@gmail.com>
2025-02-14 10:35:50 +0530

    removed backend

--- main.tf
+++ main.tf
@@ -1,12 +1,6 @@
 terraform {
   required_version = ">= 1.3.0"
 
-  backend "s3" {
-    bucket  = "my-terraform-test-bucket-2"
-    key     = "terraform.tfstate"  
-    region  = "us-east-1"
-  
-  }
 }
 
 
