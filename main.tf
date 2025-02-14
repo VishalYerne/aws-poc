806deffaff5b442ca72a5c21ee379343d56fb9c0
VishalYerne <vishaldyerne1985@gmail.com>
2025-02-14 10:50:33 +0530

    false commitment

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
 terraform {
   required_version = ">= 1.3.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.31.0"  # ✅ Correct way to specify version
+    }
+  }
 }
 
-
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
