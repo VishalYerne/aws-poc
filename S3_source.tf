a4112721f986eb43003b278219acdad7b6017ea0
VishalYerne <vishaldyerne1985@gmail.com>
2025-02-14 16:41:37 +0530

    tryingoy

--- S3_source.tf
+++ S3_source.tf
@@ -1,13 +1,11 @@
-# Create Source S3 Bucket
 resource "aws_s3_bucket" "source_bucket" {
   bucket = var.source_bucket_name
 }
 
-
-# # Upload initial.csv to Source S3 Bucket
-# resource "aws_s3_object" "initial_data" {
-#   bucket       = aws_s3_bucket.source_bucket.bucket
-#   key          = "initial_data.csv"
-#   source       = "${path.module}/data/initial_data.csv"
-#   content_type = "text/csv"
-# }
+# Upload initial.csv to Source S3 Bucket
+resource "aws_s3_object" "initial_data" {
+  bucket = aws_s3_bucket.source_bucket.id
+  key    = "initial_data.csv"
+  source = "${path.module}/data/initial_data.csv"
+  content_type = "text/csv"
+}
