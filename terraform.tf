0f8d7dd9e3f27f18210f269f02ee54ff7d927d54
VishalYerne <vishaldyerne1985@gmail.com>
2025-02-17 16:26:46 +0530

    glue role changed

--- terraform.tf
+++ terraform.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example_buck" {
-  bucket = "456767676t6t7tyyuyuysamplebucket"
-}
+# resource "aws_s3_bucket" "example_buck" {
+#   bucket = "456767676t6t7tyyuyuysamplebucket"
+# }
