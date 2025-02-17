203b0242aa87814b92d38211d2b4995ab787bf3e
VishalYerne <vishaldyerne1985@gmail.com>
2025-02-17 14:37:00 +0530

    Last

--- data/etl.py
+++ data/etl.py
@@ -5,30 +5,23 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 
-# Fetch arguments
-args = getResolvedOptions(sys.argv, ['source_bucket', 'dest_bucket'])
-
-source_bucket = args['source_bucket']
-dest_bucket = args['dest_bucket']
-
 # Initialize Glue Context
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
-job.init("glue-etl-job", args)
+job.init("glue-etl-job", getResolvedOptions(sys.argv, []))
 
 # Load Data from Source S3
 source_df = glueContext.create_dynamic_frame.from_options(
     connection_type="s3",
-    connection_options={"paths": [f"s3://{source_bucket}/initial_data.csv"]},
-    format="csv",
-    format_options={"withHeader": True, "separator": ","}
+    connection_options={"paths": ["s3://source-s3-bucket/initial_data.csv"]},
+    format="csv"
 )
 
 # Transform Data (Example: Convert to Parquet)
 transformed_df = source_df.toDF()
-transformed_df.write.mode("overwrite").parquet(f"s3://{dest_bucket}/output/")
+transformed_df.write.parquet("s3://dest-s3-bucket/output/")
 
 # Commit Glue Job
 job.commit()
