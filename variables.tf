47c2db1f5a7fd3761b963b14b7e4408fe4e11dcf
VishalYerne <vishaldyerne1985@gmail.com>
2025-02-17 16:34:15 +0530

    LOL

--- variables.tf
+++ variables.tf
@@ -3,14 +3,14 @@ variable "region" {
 }
 
 variable "source_bucket_name" {
-  default = "source-s3-bucket-98"
+  default = "source-s3-bucket-981"
 }
 
 variable "dest_bucket_name" {
-  default = "dest-s3-bucket-98"
+  default = "dest-s3-bucket-981"
 }
 
 variable "glue_role_arn" {
   description = "IAM Role ARN for AWS Glue"
-  default     = "arn:aws:iam::aws:policy/AWSGlueConsoleFullAccess"
+  default     = "arn:aws:iam::661907950294:role/glue_access_terraform"
 }
